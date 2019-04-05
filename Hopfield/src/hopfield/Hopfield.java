bbf4973f177c0952c2c1666fb8269419384eab39
CalciferPop <josehugo123@hotmail.com>
2019-04-05 16:16:52 -0600

    Avance del Hopfield, vayan checandolo

---
+++ Hopfield/src/hopfield/Hopfield.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hopfield;
+
+/**
+ *
+ * @author joseh
+ */
+public class Hopfield {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       GUI2 gui2 = new GUI2()
+               ;
+        
+        
+    }
+    
+}
